da7a76dd6dfc7bdd020444f23c83d189dbdabff2
Mostafa Dashti <mostafa.dashti@users.noreply.github.com>
2020-11-19 03:55:39 +0100

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -28,7 +28,6 @@ pip3 install --upgrade pip virtualenv
 # Account to own server process
 useradd -m -d /home/pythonapp pythonapp
 
-/opt/app/env/bin/pip3 install honcho
 # Fetch source code
 export HOME=/root
 git clone https://github.com/s258011/vibe.git /opt/app
@@ -36,6 +35,7 @@ git clone https://github.com/s258011/vibe.git /opt/app
 # Python environment setup
 virtualenv -p python3 /opt/app/env
 source /opt/app/env/bin/activate
+/opt/app/env/bin/pip3 install honcho
 /opt/app/env/bin/pip3 install -r /opt/app/requirements.txt
 
 # Set ownership to newly created account
